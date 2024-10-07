fcf860b359daa3dfc5d58d358da8617a86ec39f4
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-07 15:52:16 +0530

    add func to display all trash blogs of login user

--- app/Http/Controllers/TrashController.php
+++ app/Http/Controllers/TrashController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Trash;
 
 class TrashController extends Controller
 {
-    //
+    protected $user;
+    public function __construct()
+    {
+        $this->user = auth()->user()->id;
+    }
+
+    public function display()
+    {
+        $trash = Trash::where("user_id", $this->user)->with(['users:id,name', 'blogs'])->get();
+
+        return response()->json([
+            "status" => "success",
+            "data" => $trash
+        ]);
+    }
 }
