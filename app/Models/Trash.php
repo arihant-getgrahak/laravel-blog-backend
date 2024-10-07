374a904a4d805c01ed0fdd8d1f02cb68869d5a78
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-07 15:29:11 +0530

    write model file for Trash

--- app/Models/Trash.php
+++ app/Models/Trash.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Trash extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'user_id',
+        'blog_id',
+    ];
+
+    protected function casts()
+    {
+        return [
+            "created_at" => "datetime:Y-m-d",
+            "updated_at" => "datetime:Y-m-d",
+        ];
+    }
 }
