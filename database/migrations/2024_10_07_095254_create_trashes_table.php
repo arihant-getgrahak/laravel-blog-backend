4700848c9430bd6623c223d760592cd08feaca7a
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-07 15:25:35 +0530

    create migration for trash

--- database/migrations/2024_10_07_095254_create_trashes_table.php
+++ database/migrations/2024_10_07_095254_create_trashes_table.php
@@ -4,15 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('trashes', function (Blueprint $table) {
-            $table->id();
+            $table->uuid("id")->primary();
+            $table->foreignUuid("user_id")->constrained("users")->onDelete("cascade");
+            $table->foreignUuid("blog_id")->constrained("blogs")->onDelete("cascade");
             $table->timestamps();
         });
     }
