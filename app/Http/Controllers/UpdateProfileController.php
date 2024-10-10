fb3f99cdb455ba30ca17b3abf4fb5686fd6d266e
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-10 14:04:08 +0530

    fix: add htpps in pabbly url

--- app/Http/Controllers/UpdateProfileController.php
+++ app/Http/Controllers/UpdateProfileController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UpdateProfileRequest;
-use Storage;
 use App\Models\User;
 
 class UpdateProfileController extends Controller
@@ -13,29 +12,30 @@ class UpdateProfileController extends Controller
         $user = User::find(auth()->user()->id);
 
         $data = $request->only([
-            "name",
-            "email",
+            'name',
+            'email',
         ]);
 
         $isUpdate = $user->update(attributes: $data);
 
-        if ($request->hasFile('image')) {
+        if ($request->hasFile('profile_image')) {
             $user->clearMediaCollection('user_photo');
-            $user->addMediaFromRequest("image")->toMediaCollection('user_photo');
-            $mediaItems = $user->getMedia("user_photo");
-            $user["profile_image"] = $mediaItems[0]->original_url;
-            $user->makeHidden("media");
+            $user->addMediaFromRequest('profile_image')->toMediaCollection('user_photo');
+            $mediaItems = $user->getMedia('user_photo');
+            $user['profile_image'] = $mediaItems[0]->original_url;
+            $user->makeHidden('media');
         }
         if ($isUpdate) {
             return response()->json([
-                "status" => true,
-                "message" => "Profile updated successfully",
-                "data" => $user
+                'status' => true,
+                'message' => 'Profile updated successfully',
+                'data' => $user,
             ], 200);
         }
+
         return response()->json([
-            "status" => false,
-            "message" => "Unable to update profile",
+            'status' => false,
+            'message' => 'Unable to update profile',
         ], 500);
 
     }
