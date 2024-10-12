151a0f5990d2ae39e03b2bfe2104c5b47425745b
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-12 12:11:41 +0530

    feat: add file to test browser test

---
+++ tests/Unit/BrowserTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class BrowserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
