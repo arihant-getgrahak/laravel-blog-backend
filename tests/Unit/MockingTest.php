dbd471194244c0cd5d51ce56a98b6c4c82df6d66
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-12 12:10:21 +0530

    feat: write test code for mocking

--- tests/Unit/MockingTest.php
+++ tests/Unit/MockingTest.php
@@ -2,22 +2,39 @@
 
 namespace Tests\Unit;
 
-use App\Service;
-use Mockery;
-use Mockery\MockInterface;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Cache;
 use Tests\TestCase;
 
 // use ServiceProvider;
 
 class MockingTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_something_can_be_mocked(): void
     {
-        $this->instance(
-            Service::class,
-            Mockery::mock(Service::class, function (MockInterface $mock) {
-                $mock->shouldReceive('process')->once();
-            })
-        );
+        Cache::spy();
+
+        $response = $this->get('/api');
+
+        $response->assertStatus(200);
+
+        Cache::shouldReceive('put')->with('name', 'Taylor', 10);
     }
 }
+
+// $this->travel(5)->milliseconds();
+// $this->travel(5)->seconds();
+// $this->travel(5)->minutes();
+// $this->travel(5)->hours();
+// $this->travel(5)->days();
+// $this->travel(5)->weeks();
+// $this->travel(5)->years();
+
+// $this->travel(-5)->hours();
+
+// $this->travelTo(now()->subHours(6));
+
+// // Return back to the present time...
+// $this->travelBack();
